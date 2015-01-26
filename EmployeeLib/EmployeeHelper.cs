f5cd6b862d8699bca013f9acb5f3d3b951a0c9fe
jespinozabol <jose.espinoza@janeirodigital.com>
2015-01-26 11:41:42 -0600

    first commit

---
+++ EmployeeLib/EmployeeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLib
+{
+    public class EmployeeHelper
+    {
+        public Employee GetByEmployeeID(int empId)
+        {
+            NorthwindEntities db=new NorthwindEntities();
+            var data = from item in db.Employees
+                       where item.EmployeeID == empId
+                       select item;
+            return data.SingleOrDefault();
+        }
+    }
+}
---
+++ UnitTestsDemo/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace UnitTestsDemo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestGetEmployeeByID()
+        {
+            EmployeeLib.EmployeeHelper helper = new EmployeeLib.EmployeeHelper();
+            EmployeeLib.Employee emp = helper.GetByEmployeeID(100);
+            Assert.IsNotNull(emp);
+        }
+    }
+}
